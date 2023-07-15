0181d5a2764cec40882512c6c778c23157f41172
Yuri Watanabe <yuriwatanabe9090@gmail.com>
2023-07-15 09:32:19 -0300

    Starting project

---
+++ src/main/java/dev/ctc/learning/rappellemoiapi/RappellemoiApiApplication.java
@@ -0,0 +1,13 @@
+package dev.ctc.learning.rappellemoiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RappellemoiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RappellemoiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ctc/learning/rappellemoiapi/RappellemoiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ctc.learning.rappellemoiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RappellemoiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
